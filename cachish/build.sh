dbce02c9dd4a9f35f0dd2bca7b67828e8799f745
Tarjei Husøy <git@thusoy.com>
2017-01-04 18:46:34 +0100

    Add cachish

---
+++ cachish/build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -eu
+
+REPO=thusoy/cachish
+COMMITISH=v1.0.0
+
+main () {
+    get_source
+    build_deb
+}
+
+get_source () {
+    local source_tarball=$(mktemp)
+    curl "https://api.github.com/repos/$REPO/tarball/$COMMITISH" \
+        --location \
+        --silent \
+        -H 'Accept: application/vnd.github.v3.raw' \
+        -o "$source_tarball"
+    tempdir=$(mktemp -d)
+    trap 'rm -rf "$tempdir"' INT TERM EXIT
+    tar xf "$source_tarball" -C "$tempdir"
+}
+
+build_deb () {
+    cd "$tempdir/"*
+    local build_dir=$(pwd)
+    ./tools/build_deb.sh
+    cd -
+    mkdir -p dist
+    mv "$build_dir/dist/"*.deb dist/
+}
+
+main
